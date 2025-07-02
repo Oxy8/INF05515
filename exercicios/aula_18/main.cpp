2fb3593ea01bc974c5d7bf48eb62509de2a24410
Oxy8 <34687508+Oxy8@users.noreply.github.com>
2025-07-02 16:34:39 -0300

    aula18

--- exercicios/aula_18/main.cpp
+++ exercicios/aula_18/main.cpp
@@ -10,13 +10,24 @@ Conjunto independente
 grafo linear
 Para ser Conjunto independente precisamos que dois vértices não sejam adjacentes.
 Precisamos achar com peso máximo.
-podemos sacrificar cobrir algumas arestas desde que consigamos pesos altos, então não intercala.
+podemos sacrificar cobrir algumas Vertices desde que consigamos pesos altos, então não intercala.
 */
 
+
+// Recebe Vertices
 void conjunto_independente_maximo(const std::vector<int>& pesos) {   
 
     int n = pesos.size();
 
+    if (n == 0) {
+        std::cout << "Peso Máximo: 0" << std::endl;
+        return;
+    }
+    if (n == 1) {
+        std::cout << "Peso máximo: " << pesos[1] << std::endl;
+        std::cout << "Vertice: " << 1 << " peso: " << pesos[0] << std::endl;
+        return;
+    }
 
     std::vector<int> dp(n);
     dp[0] = pesos[0];
@@ -32,11 +43,11 @@ void conjunto_independente_maximo(const std::vector<int>& pesos) {
     for (int i = n - 1; i >= 0; ) {
         
         if (i == 0) {
-            std::cout << "Aresta: " << i << " peso: " << pesos[i] << std::endl;
+            std::cout << "Vertice: " << i << " peso: " << pesos[i-1] << std::endl;
             break; 
         }
         if (dp[i] > dp[i-1]) {
-            std::cout << "Aresta: " << i << " peso: " << pesos[i] << std::endl;
+            std::cout << "Vertice: " << i << " peso: " << pesos[i-1] << std::endl;
             i -= 2; 
         } else {
             i -= 1; 
@@ -46,7 +57,6 @@ void conjunto_independente_maximo(const std::vector<int>& pesos) {
 
 int main() {
     std::vector<int> pesos = {5, 2, 10, 8, 3};
-    // 5 pesos são 6 nodos
     conjunto_independente_maximo(pesos);
 
     std::cout << "\n\n\n\n\n\n";
