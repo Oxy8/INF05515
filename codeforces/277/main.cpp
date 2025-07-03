86c9a9eb0f425f12dabe5d80176ce9c8740da79b
Oxy8 <34687508+Oxy8@users.noreply.github.com>
2025-07-02 21:00:37 -0300

    aula7

--- codeforces/277/main.cpp
+++ codeforces/277/main.cpp
@@ -1,3 +1,27 @@
+/*
+n = numero de pessoas
+m = numero de linguas
+
+Vamos considerar o pior caso com sendo o caso quando todas pessoas falam todas linguas.
+
+Construimos vetores onde são guardados cada uma das pessoas que falam cada lingua. (n*m) no pior caso
+
+Iteramos cada vetor de linguagens e adicionamos arestas conectando todos os membros entre si (n*m) no pior caso
+
+iteramos cada pessoas, executando BFS em cada uma.
+Poderíamos considerar que como complexidade de BFS é O(V+E) e ele é executado V vezes (cada pessoa é um vértices).
+então a complexidade seria O(n*(n+E)), mas o BFS só executa um número equivalente ao numero de grafos disjuntos
+presentes, então só iteraria por todos os nodos se só rodasse 1 vez também. a soma da complexidade de todos os bfs
+executados será O(V+E). No pior dos casos, O(V+E) corresponde a O(n+n*m)
+
+Então a complexidade final é, no pior dos casos
+= O(n+n*m + n*m + n*m)
+= O(n+3*n*m)
+= O(n*m)
+*/
+
+
+
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -52,7 +76,7 @@ int main() {
     std::vector<std::vector<int>> adj(n + 1);
     for (int lang = 1; lang <= m; ++lang) {
         for (size_t i = 1; i < lang_to_employees[lang].size(); ++i) {
-            int u = lang_to_employees[lang][0]; // Connect all to the first person
+            int u = lang_to_employees[lang][0]; 
             int v = lang_to_employees[lang][i];
             adj[u].push_back(v);
             adj[v].push_back(u);
@@ -74,3 +98,4 @@ int main() {
 
     return 0;
 }
+
